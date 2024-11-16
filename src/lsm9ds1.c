aecf3fc2f4683d36dc9e23e4358b465887ef2e61
sid-bhargav <sid.bhargav@gmail.com>
2024-11-16 01:42:28 -0600

    src half written

--- src/lsm9ds1.c
+++ src/lsm9ds1.c
@@ -9,3 +9,27 @@
 #include "esp_check.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#define I2C_EEPROM_MAX_TRANS_UNIT (48)
+// Maximum number of bytes that can be transferred in a single transaction.
+// Different devices might share one I2C bus
+
+static const char TAG[] = "i2c-lsm9ds1";
+
+esp_err_t i2c_lsm9ds1_init(i2c_master_bus_handle_t bus_handle, const i2c_lsm9ds1_config_t *lsm9ds1_config, i2c_lsm9ds1_handle_t *lsm9ds1_handle)
+{
+    esp_err_t ret = ESP_OK;
+    i2c_lsm9ds1_handle_t out_handle;
+    out_handle = (i2c_lsm9ds1_handle_t)calloc(1, sizeof(*out_handle));
+    ESP_GOTO_ON_FALSE(out_handle, ESP_ERR_NO_MEM, err, TAG, "no memory for i2c lsm9ds1 device");
+
+    return ESP_OK;
+
+err:
+    if (out_handle && out_handle->i2c_dev)
+    {
+        i2c_master_bus_rm_device(out_handle->i2c_dev);
+    }
+    free(out_handle);
+    return ret;
+}
